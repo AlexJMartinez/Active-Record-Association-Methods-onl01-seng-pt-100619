aedc2cc7a8ec4e7fd6899b96b283d862378dc39e
Alex Martinez <alexandr3martin3z@gmail.com>
2020-01-14 04:59:50 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,6 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
 
-    self.artist = Artist.find_or_create_by(name: "Drake")
-    
+    self.artist = Artist.find_or_create_by(name: "Drake")    
   end
 end
